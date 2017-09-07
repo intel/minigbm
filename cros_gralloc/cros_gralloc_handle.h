b3deb507356678e171838e6a39afe7d440c8c44d
Harish Krupo <harish.krupo.kps@intel.com>
2017-09-06 17:31:44 -0700

    AndroidIA: Add information required for media.
    Change-Id: Ib818412df9a752835ba2bc91568fb602f33a2a75
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

--- cros_gralloc/cros_gralloc_handle.h
+++ cros_gralloc/cros_gralloc_handle.h
@@ -33,6 +33,16 @@ struct cros_gralloc_handle {
 	int32_t usage; /* Android usage. */
 	uint32_t consumer_usage;
 	uint32_t producer_usage;
+	uint32_t yuv_color_range;   // YUV Color range.
+	uint32_t is_updated;        // frame updated flag
+	uint32_t is_encoded;        // frame encoded flag
+	uint32_t is_encrypted;
+	uint32_t is_key_frame;
+	uint32_t is_interlaced;
+	uint32_t is_mmc_capable;
+	uint32_t compression_mode;
+	uint32_t compression_hint;
+	uint32_t codec;
 };
 
 typedef const struct cros_gralloc_handle *cros_gralloc_handle_t;
