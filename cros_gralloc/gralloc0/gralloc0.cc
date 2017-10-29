me176c-dev/android_external_minigbm

Android-IA: Add support to easily add new formats.
Camera, Media use cases seem to need lot of custom
formats depending on hardware support. This patch
squashes earlies work done for NV12, NV12 tiled etc
into one and also makes it easy to isolate these
custom formats into i915_private_* files, rather
than spreading them over whole code base.

Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>
Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
Signed-off-by: Yaodong Li <yaodong.li@intel.com>
Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>
Signed-off-by: Lin Johnson <lin.johnson@intel.com>

cros_gralloc/gralloc0/gralloc0.cc
@@ -9,6 +9,9 @@
 #include <hardware/gralloc.h>
 #include <memory.h>
 
+#include "../i915_private_android.h"
+#include "../i915_private_android_types.h"
+
 struct gralloc0_module {
 	gralloc_module_t base;
 	std::unique_ptr<alloc_device_t> alloc;
@@ -37,7 +40,7 @@ static int64_t gralloc0_convert_flags(int flags)
 	uint64_t usage = BO_USE_NONE;
 
 	if (flags & GRALLOC_USAGE_CURSOR)
-		usage |= BO_USE_NONE;
+                usage |= BO_USE_CURSOR;
 	if ((flags & GRALLOC_USAGE_SW_READ_MASK) == GRALLOC_USAGE_SW_READ_RARELY)
 		usage |= BO_USE_SW_READ_RARELY;
 	if ((flags & GRALLOC_USAGE_SW_READ_MASK) == GRALLOC_USAGE_SW_READ_OFTEN)
@@ -102,7 +105,7 @@ static int gralloc0_alloc(alloc_device_t *dev, int w, int h, int format, int usa
 	if (!supported) {
 		cros_gralloc_error("Unsupported combination -- HAL format: %u, HAL flags: %u, "
 				   "drv_format: %4.4s, drv_flags: %llu",
-				   format, usage, reinterpret_cast<char *>(&descriptor.drm_format),
+                                   format, usage, drmFormat2Str(descriptor.drm_format),
 				   static_cast<unsigned long long>(descriptor.drv_usage));
 		return -EINVAL;
 	}
@@ -330,7 +333,8 @@ static int gralloc0_lock_async_ycbcr(struct gralloc_module_t const *module, buff
 
 	if ((hnd->droid_format != HAL_PIXEL_FORMAT_YCbCr_420_888) &&
 	    (hnd->droid_format != HAL_PIXEL_FORMAT_YV12) &&
-	    (hnd->droid_format != HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED)) {
+            (hnd->droid_format != HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED) &&
+	     !i915_private_supported_yuv_format(hnd->droid_format)) {
 		cros_gralloc_error("Non-YUV format not compatible.");
 		return -EINVAL;
 	}
@@ -342,6 +346,7 @@ static int gralloc0_lock_async_ycbcr(struct gralloc_module_t const *module, buff
 
 	switch (hnd->format) {
 	case DRM_FORMAT_NV12:
+	case DRM_FORMAT_NV12_Y_TILED_INTEL:
 		ycbcr->y = addr[0];
 		ycbcr->cb = addr[1];
 		ycbcr->cr = addr[1] + 1;

i915.c
@@ -16,6 +16,7 @@
 #include "drv_priv.h"
 #include "helpers.h"
 #include "util.h"
+#include "i915_private.h"
 
 #define I915_CACHELINE_SIZE 64
 #define I915_CACHELINE_MASK (I915_CACHELINE_SIZE - 1)
@@ -34,6 +35,8 @@ static const uint32_t texture_source_formats[] = { DRM_FORMAT_YVU420, DRM_FORMAT
 struct i915_device {
 	uint32_t gen;
 	int32_t has_llc;
+	uint64_t cursor_width;
+	uint64_t cursor_height;
 };
 
 static uint32_t i915_get_gen(int device_id)
@@ -164,6 +167,8 @@ static int i915_add_combinations(struct driver *drv)
 	if (ret)
 		return ret;
 
+	i915_private_add_combinations(drv);
+
 	items = drv_query_kms(drv, &num_items);
 	if (!items || !num_items)
 		return 0;
@@ -203,8 +208,8 @@ static int i915_align_dimensions(struct bo *bo, uint32_t tiling, uint32_t *strid
 			horizontal_alignment = 512;
 			vertical_alignment = 8;
 		} else {
-			horizontal_alignment = 128;
-			vertical_alignment = 32;
+                       horizontal_alignment = 128;
+                       vertical_alignment = 32;
 		}
 		break;
 	}
@@ -223,6 +228,8 @@ static int i915_align_dimensions(struct bo *bo, uint32_t tiling, uint32_t *strid
 		vertical_alignment *= 2;
 		break;
 	}
+
+	i915_private_align_dimensions(bo->format, &vertical_alignment);
 
 	*aligned_height = ALIGN(bo->height, vertical_alignment);
 	if (i915->gen > 3) {
@@ -287,6 +294,8 @@ static int i915_init(struct driver *drv)
 
 	drv->priv = i915;
 
+	i915_private_init(drv, &i915->cursor_width, &i915->cursor_height);
+
 	return i915_add_combinations(drv);
 }
 
@@ -298,6 +307,7 @@ static int i915_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32
 	uint32_t stride;
 	struct drm_i915_gem_create gem_create;
 	struct drm_i915_gem_set_tiling gem_set_tiling;
+	struct i915_device *i915_dev = (struct i915_device *)bo->drv->priv;
 	struct combination *combo;
 
 	combo = drv_get_combination(bo->drv, format, flags);
@@ -308,9 +318,19 @@ static int i915_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32
 
 	stride = drv_stride_from_format(format, width, 0);
 
-	ret = i915_align_dimensions(bo, bo->tiling, &stride, &height);
-	if (ret)
-		return ret;
+        /*
+         * Align cursor width and height to values expected by Intel
+         * HW.
+         */
+        if (flags & BO_USE_CURSOR) {
+            width = ALIGN(width, i915_dev->cursor_width);
+            height = ALIGN(height, i915_dev->cursor_height);
+            stride = drv_stride_from_format(format, width, 0);
+        } else {
+            ret = i915_align_dimensions(bo, bo->tiling, &stride, &height);
+            if (ret)
+                return ret;
+        }
 
 	/*
 	 * HAL_PIXEL_FORMAT_YV12 requires the buffer height not be aligned, but we need to keep
@@ -349,6 +369,12 @@ static int i915_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32
 	 */
 	if (bo->tiling == I915_TILING_NONE)
 		bo->total_size += 64;
+
+        /*
+         * Ensure we pass aligned width/height.
+         */
+        bo->width = width;
+        bo->height = height;
 
 	memset(&gem_create, 0, sizeof(gem_create));
 	gem_create.size = bo->total_size;
@@ -481,11 +507,17 @@ static int i915_bo_unmap(struct bo *bo, struct map_info *data)
 
 static uint32_t i915_resolve_format(uint32_t format, uint64_t usage)
 {
+	uint32_t resolved_format;
+	if (i915_private_resolve_format(format, usage, &resolved_format)) {
+	    return resolved_format;
+	}
+
 	switch (format) {
 	case DRM_FORMAT_FLEX_IMPLEMENTATION_DEFINED:
 		/* KBL camera subsystem requires NV12. */
 		if (usage & (BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE))
 			return DRM_FORMAT_NV12;
+
 		/*HACK: See b/28671744 */
 		return DRM_FORMAT_XBGR8888;
 	case DRM_FORMAT_FLEX_YCbCr_420_888:

intel/minigbm

Signed-off-by: Yin Zhiye <zhiyex.yin@intel.com>

cros_gralloc/gralloc0/gralloc0.cc
@@ -9,6 +9,9 @@
 #include <hardware/gralloc.h>
 #include <memory.h>
 
+#include "../i915_private_android.h"
+#include "../i915_private_android_types.h"
+
 struct gralloc0_module {
 	gralloc_module_t base;
 	std::unique_ptr<alloc_device_t> alloc;
@@ -37,7 +40,7 @@ static uint64_t gralloc0_convert_usage(int usage)
 	uint64_t use_flags = BO_USE_NONE;
 
 	if (usage & GRALLOC_USAGE_CURSOR)
-		use_flags |= BO_USE_NONE;
+		use_flags |= BO_USE_CURSOR;
 	if ((usage & GRALLOC_USAGE_SW_READ_MASK) == GRALLOC_USAGE_SW_READ_RARELY)
 		use_flags |= BO_USE_SW_READ_RARELY;
 	if ((usage & GRALLOC_USAGE_SW_READ_MASK) == GRALLOC_USAGE_SW_READ_OFTEN)
@@ -114,7 +117,7 @@ static int gralloc0_alloc(alloc_device_t *dev, int w, int h, int format, int usa
 	if (!supported) {
 		cros_gralloc_error("Unsupported combination -- HAL format: %u, HAL usage: %u, "
 				   "drv_format: %4.4s, use_flags: %llu",
-				   format, usage, reinterpret_cast<char *>(&descriptor.drm_format),
+				   format, usage, drmFormat2Str(descriptor.drm_format),
 				   static_cast<unsigned long long>(descriptor.use_flags));
 		return -EINVAL;
 	}
@@ -342,7 +345,8 @@ static int gralloc0_lock_async_ycbcr(struct gralloc_module_t const *module, buff
 
 	if ((hnd->droid_format != HAL_PIXEL_FORMAT_YCbCr_420_888) &&
 	    (hnd->droid_format != HAL_PIXEL_FORMAT_YV12) &&
-	    (hnd->droid_format != HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED)) {
+            (hnd->droid_format != HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED) &&
+	     !i915_private_supported_yuv_format(hnd->droid_format)) {
 		cros_gralloc_error("Non-YUV format not compatible.");
 		return -EINVAL;
 	}
@@ -354,6 +358,7 @@ static int gralloc0_lock_async_ycbcr(struct gralloc_module_t const *module, buff
 
 	switch (hnd->format) {
 	case DRM_FORMAT_NV12:
+	case DRM_FORMAT_NV12_Y_TILED_INTEL:
 		ycbcr->y = addr[0];
 		ycbcr->cb = addr[1];
 		ycbcr->cr = addr[1] + 1;

i915.c
@@ -16,6 +16,7 @@
 #include "drv_priv.h"
 #include "helpers.h"
 #include "util.h"
+#include "i915_private.h"
 
 #define I915_CACHELINE_SIZE 64
 #define I915_CACHELINE_MASK (I915_CACHELINE_SIZE - 1)
@@ -35,6 +36,8 @@ static const uint32_t texture_source_formats[] = { DRM_FORMAT_YVU420, DRM_FORMAT
 struct i915_device {
 	uint32_t gen;
 	int32_t has_llc;
+	uint64_t cursor_width;
+	uint64_t cursor_height;
 };
 
 static uint32_t i915_get_gen(int device_id)
@@ -165,6 +168,8 @@ static int i915_add_combinations(struct driver *drv)
 	if (ret)
 		return ret;
 
+	i915_private_add_combinations(drv);
+
 	items = drv_query_kms(drv, &num_items);
 	if (!items || !num_items)
 		return 0;
@@ -204,8 +209,8 @@ static int i915_align_dimensions(struct bo *bo, uint32_t tiling, uint32_t *strid
 			horizontal_alignment = 512;
 			vertical_alignment = 8;
 		} else {
-			horizontal_alignment = 128;
-			vertical_alignment = 32;
+                       horizontal_alignment = 128;
+                       vertical_alignment = 32;
 		}
 		break;
 	}
@@ -224,6 +229,8 @@ static int i915_align_dimensions(struct bo *bo, uint32_t tiling, uint32_t *strid
 		vertical_alignment *= 2;
 		break;
 	}
+
+	i915_private_align_dimensions(bo->format, &vertical_alignment);
 
 	*aligned_height = ALIGN(bo->height, vertical_alignment);
 	if (i915->gen > 3) {
@@ -288,6 +295,8 @@ static int i915_init(struct driver *drv)
 
 	drv->priv = i915;
 
+	i915_private_init(drv, &i915->cursor_width, &i915->cursor_height);
+
 	return i915_add_combinations(drv);
 }
 
@@ -299,6 +308,7 @@ static int i915_bo_create_for_modifier(struct bo *bo, uint32_t width, uint32_t h
 	uint32_t stride;
 	struct drm_i915_gem_create gem_create;
 	struct drm_i915_gem_set_tiling gem_set_tiling;
+	struct i915_device *i915_dev = (struct i915_device *)bo->drv->priv;
 
 	switch (modifier) {
 	case DRM_FORMAT_MOD_LINEAR:
@@ -314,9 +324,19 @@ static int i915_bo_create_for_modifier(struct bo *bo, uint32_t width, uint32_t h
 
 	stride = drv_stride_from_format(format, width, 0);
 
-	ret = i915_align_dimensions(bo, bo->tiling, &stride, &height);
-	if (ret)
-		return ret;
+        /*
+         * Align cursor width and height to values expected by Intel
+         * HW.
+         */
+	if (bo->use_flags & BO_USE_CURSOR) {
+            width = ALIGN(width, i915_dev->cursor_width);
+            height = ALIGN(height, i915_dev->cursor_height);
+            stride = drv_stride_from_format(format, width, 0);
+        } else {
+            ret = i915_align_dimensions(bo, bo->tiling, &stride, &height);
+            if (ret)
+                return ret;
+        }
 
 	/*
 	 * HAL_PIXEL_FORMAT_YV12 requires the buffer height not be aligned, but we need to keep
@@ -355,6 +375,12 @@ static int i915_bo_create_for_modifier(struct bo *bo, uint32_t width, uint32_t h
 	 */
 	if (bo->tiling == I915_TILING_NONE)
 		bo->total_size += 64;
+
+        /*
+         * Ensure we pass aligned width/height.
+         */
+        bo->width = width;
+        bo->height = height;
 
 	memset(&gem_create, 0, sizeof(gem_create));
 	gem_create.size = bo->total_size;
@@ -530,6 +556,11 @@ static int i915_bo_flush(struct bo *bo, struct map_info *data)
 
 static uint32_t i915_resolve_format(uint32_t format, uint64_t use_flags)
 {
+	uint32_t resolved_format;
+	if (i915_private_resolve_format(format, use_flags, &resolved_format)) {
+	    return resolved_format;
+	}
+
 	switch (format) {
 	case DRM_FORMAT_FLEX_IMPLEMENTATION_DEFINED:
 		/* KBL camera subsystem requires NV12. */