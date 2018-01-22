be7cc5a5f3c0ad0fb0e03cc3a6f6198873e46852
Yin.ZhiyeX <zhiyex.yin@intel.com>
2018-01-22 13:36:57 +0800

    fix buffer overflow when copy NV12 to YVU420
    YVU420 plane number and UV plane length diff with NV12, crash when
    copying data.
    
    Jira: https://jira01.devtools.intel.com/browse/OAM-55982
    Test: android.hardware.camera2.cts.ImageWriterTest#
          testYuvImageWriterReaderOperation
    
    Signed-off-by: Yin, ZhiyeX <zhiyex.yin@intel.com>

--- virtio_gpu.c
+++ virtio_gpu.c
@@ -15,7 +15,7 @@ static const uint32_t render_target_formats[] = { DRM_FORMAT_ABGR8888, DRM_FORMA
 						  DRM_FORMAT_RGB565, DRM_FORMAT_XBGR8888,
 						  DRM_FORMAT_XRGB8888 };
 
-static const uint32_t texture_source_formats[] = { DRM_FORMAT_R8, DRM_FORMAT_YVU420,
+static const uint32_t texture_source_formats[] = { DRM_FORMAT_R8, DRM_FORMAT_NV12, DRM_FORMAT_YVU420,
 						   DRM_FORMAT_YVU420_ANDROID };
 
 static int virtio_gpu_init(struct driver *drv)
