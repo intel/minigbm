822bc4b3a0e1293ad96dc382417e9e10e5f1ae2b
renchenglei <chenglei.ren@intel.com>
2019-07-13 11:34:53 +0800

    Add pixel format RGBA_FP16 and RGBA_1010102 support
    We add two pixel formats HAL_PIXEL_FORMAT_RGBA_FP16 and
    HAL_PIXEL_FORMAT_RGBA_1010102 to minigbm, which could help
    to pass all dEQP-VK.wsi.* issue.
    
    Test: All dEQP-VK.wsi.* case pass and no regression on other CTS test
    Change-Id: I21739f9c7001c7d4b21cc6e92edc9ea9072ffd1a
    Tracked-On: OAM-84250
    Signed-off-by: Chenglei Ren <chenglei.ren@intel.com>

--- cros_gralloc/i915_private_android.cc
+++ cros_gralloc/i915_private_android.cc
@@ -34,6 +34,10 @@ uint32_t i915_private_convert_format(int format)
 		return DRM_FORMAT_YUV422;
 	case HAL_PIXEL_FORMAT_P010_INTEL:
 		return DRM_FORMAT_P010;
+	case HAL_PIXEL_FORMAT_RGBA_1010102:
+		return DRM_FORMAT_ABGR2101010;
+	case HAL_PIXEL_FORMAT_RGBA_FP16:
+		return DRM_FORMAT_ABGR16161616F;
 	}
 
 	return DRM_FORMAT_NONE;
@@ -77,6 +81,10 @@ int32_t i915_private_invert_format(int format)
 		return HAL_PIXEL_FORMAT_YCbCr_422_SP;
 	case DRM_FORMAT_YUV422:
 		return HAL_PIXEL_FORMAT_YCbCr_422_888;
+	case DRM_FORMAT_ABGR2101010:
+		return HAL_PIXEL_FORMAT_RGBA_1010102;
+	case DRM_FORMAT_ABGR16161616F:
+		return HAL_PIXEL_FORMAT_RGBA_FP16;
 	default:
 		cros_gralloc_error("Unhandled DRM format %4.4s", drmFormat2Str(format));
 	}
