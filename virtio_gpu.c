107d9ef5ad40806336f6ca0d71dc9d91ccd42261
renchenglei <chenglei.ren@intel.com>
2021-04-06 23:54:39 +0800

    INTERNAL: Add camera usage for RGBA format
    On virgl backend, we use vhal camera. During camera
    preview, we use RGBA format, so add camera usage for
    it.
    
    Tracked-On: OAM-96725
    Signed-off-by: Ren Chenglei chenglei.ren@intel.com

--- virtio_gpu.c
+++ virtio_gpu.c
@@ -638,6 +638,10 @@ static int virtio_gpu_init(struct driver *drv)
 			       BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE | BO_USE_HW_VIDEO_DECODER);
 	drv_modify_combination(drv, DRM_FORMAT_R16, &LINEAR_METADATA,
 			       BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE | BO_USE_HW_VIDEO_DECODER);
+#ifdef USE_GRALLOC1
+	drv_modify_combination(drv, DRM_FORMAT_ABGR8888, &LINEAR_METADATA,
+			       BO_USE_CAMERA_WRITE | BO_USE_CAMERA_READ);
+#endif
 
 	return drv_modify_linear_combinations(drv);
 }
